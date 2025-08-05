f93ef710ad9c1f631a614b6e8dd65fdcc875d396
mahilmithranks <mahilmithranks2007@gmail.com>
2025-08-05 16:13:36 +0530

    done

--- frontend/vite-project/src/utils/uploadthing.js
+++
@@ -1,8 +0,0 @@
-import { generateReactHelpers } from "@uploadthing/react";
-
-/**
- * This file generates the custom React hooks for our application,
- * which is the standard way to use UploadThing in recent versions.
- * We then import these generated helpers into our components.
- */
-export const { useUploadThing, uploadFiles } = generateReactHelpers();
